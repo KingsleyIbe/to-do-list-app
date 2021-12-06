dc306c8cda751f34e199e6eceddd03c833c55d98
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-12-06 06:56:03 +0100

    Add html file the root directory

--- src/ultils/toggle-theme.js
+++ src/ultils/toggle-theme.js
@@ -1,13 +1,13 @@
 const toggleTheme = () => {
   document.querySelector('.toggle-dark-mode').innerHTML = `
-  <i class="fas fa-moon fa-rotate-100">Dark mode</i>
+  <span><i class="fas fa-moon fa-rotate-100">Dark mode</i></span>
   `
 
   const toggleDark = document.querySelector('.fa-moon');
   toggleDark.addEventListener('click', () => {
     toggleDark.innerHTML = `<i class="fas fa-sun">Light mode</i>`
     const element = document.body;
-    element.classList.toggle('dark-mode');
+    element.classList.toggle('dark-mode')
 })
   }
 
